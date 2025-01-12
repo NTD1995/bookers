899ccf7747291cfe8b644d7e565e84dc2b5efb86
abedaiki <abe12261994@gmail.com>
2025-01-12 07:21:41 +0000

    編集機能の作成

--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,14 @@ Rails.application.routes.draw do
   # get 'books/top'
   # get 'books/new'
   # get '/books' => 'books#index'
-  # get 'books/edit'
   #  post 'books' => 'books#create'
   #  get '/' => 'books#index'
     get '/top' => 'homes#top'
      root to: 'books#index'
       # .../lists/1 や .../lists/3 に該当する
   get 'books/:id' => 'books#show' , as: 'book'
+   get 'books/:id/edit' => 'books#edit', as: 'edit_book'
+    patch 'books/:id' => 'books#update', as: 'update_book'
    resources :books
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
