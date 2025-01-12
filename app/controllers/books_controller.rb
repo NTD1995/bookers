d590c1c41d5e823bf9d2f39c8314dd18373360c1
abedaiki <abe12261994@gmail.com>
2025-01-12 01:50:21 +0000

    投稿機能の作成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,15 +2,34 @@ class BooksController < ApplicationController
   def top
   end
   def new
+      @book = Book.new
   end
 
   def index
-     @book = Book.new
   end
 
   def show
   end
 
   def edit
+  end
+
+  def create
+    # １.&2. データを受け取り新規登録するためのインスタンス作成
+    book = Book.new(book_params)
+    # 3. データをデータベースに保存するためのsaveメソッド実行
+    book.save
+    # 4. トップ画面へリダイレクト
+    redirect_to '/top'
+  end
+  
+  :
+  :省略
+  :
+  
+  private
+  # ストロングパラメータ
+  def book_params
+    params.require(:book).permit(:title, :body)
   end
 end
