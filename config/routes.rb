8dd8bae6c9ff0152a1a1fb84e40d734dd81efcf1
abedaiki <abe12261994@gmail.com>
2025-01-12 06:03:49 +0000

    投稿機能の修正とtopページの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
-  get 'books/top'
-  get 'books/new'
-  get '/books' => 'books#index'
-  get 'books/show'
-  get 'books/edit'
-  get '/top' => 'homes#top'
-   post 'books' => 'books#create'
+  # get 'books/top'
+  # get 'books/new'
+  # get '/books' => 'books#index'
+  # get 'books/show'
+  # get 'books/edit'
+  #  post 'books' => 'books#create'
+  #  get '/' => 'books#index'
+   root to: 'books#index'
+    get '/top' => 'homes#top'
+   resources :books
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
