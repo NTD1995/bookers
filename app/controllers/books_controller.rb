082d51315958f3e5c0d0be6f52bc07246b0c3ea2
abedaiki <abe12261994@gmail.com>
2025-01-12 06:40:39 +0000

    詳細画面の作成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -11,6 +11,7 @@ class BooksController < ApplicationController
   end
 
   def show
+     @book = Book.find(params[:id])  
   end
 
   def edit
@@ -21,8 +22,8 @@ class BooksController < ApplicationController
     book = Book.new(book_params)
     # 3. データをデータベースに保存するためのsaveメソッド実行
     book.save
-    # 4. トップ画面へリダイレクト
-    redirect_to '/top'
+    # 4. 詳細画面へリダイレクト
+    redirect_to book_path(book.id)
   end
   
   private
--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,13 @@ Rails.application.routes.draw do
   # get 'books/top'
   # get 'books/new'
   # get '/books' => 'books#index'
-  # get 'books/show'
   # get 'books/edit'
   #  post 'books' => 'books#create'
   #  get '/' => 'books#index'
-   root to: 'books#index'
     get '/top' => 'homes#top'
+     root to: 'books#index'
+      # .../lists/1 や .../lists/3 に該当する
+  get 'books/:id' => 'books#show' , as: 'book'
    resources :books
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
