3606d685ab1160588cecfcf33d455e511702054f
abedaiki <abe12261994@gmail.com>
2025-01-13 01:58:38 +0000

    rspecによるテスト

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
     get '/top' => 'homes#top'
-     root to: 'books#index'
+     root to: 'homes#top'
   get 'books/:id' => 'books#show' , as: 'book'
    get 'books/:id/edit' => 'books#edit', as: 'edit_book'
     patch 'books/:id' => 'books#update', as: 'update_book'
